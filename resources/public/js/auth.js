bd8f24aa98fcb1e6ab0e91d17f94578224643ebe
ferrucc-io <hi@ferrucc.io>
2018-10-01 12:48:47 +0200

    Added Authentication in HTML
    Added authentication views as HTML, need to convert them in Clojure code

---
+++ resources/public/js/auth.js
@@ -0,0 +1,33 @@
+const lineEq = (y2, y1, x2, x1, currentVal) => {
+    // y = mx + b
+    var m = (y2 - y1) / (x2 - x1), b = y1 - m * x1;
+    return m * currentVal + b;
+};
+
+const validateEmail = (email) => {
+    var re = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const form = document.querySelector('.auth__form');
+const submitBttn = form.querySelector('.form__button');
+const requiredElems = Array.from(form.querySelectorAll('input[required]'));
+
+// whatever we do, start at [distanceThreshold.max]px from the element and end at [distanceThreshold.min]px from the element.
+const distanceThreshold = {min: 0, max: 75};
+const opacityInterval = {from: 0, to: 1};
+
+new Nearby(submitBttn, {
+    onProgress: (distance) => {
+        const o = lineEq(opacityInterval.from, opacityInterval.to, distanceThreshold.max, distanceThreshold.min, distance);
+
+        requiredElems.forEach((el) => {
+            if ( !el.value || el.type === 'email' && !validateEmail(el.value) ) {
+                inputErrorEl = el.nextElementSibling;
+                TweenMax.to(inputErrorEl, .3, {
+                    opacity: Math.max(o,opacityInterval.from)
+                });
+            }
+        });
+    }
+});
