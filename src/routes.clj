fcb422d8673db991c7f418127eab0dc89b39fdc2
Sean Walker <swlkr@fastmail.com>
2018-10-02 09:12:00 -0700

    Route change

--- src/routes.clj
+++ src/routes.clj
@@ -5,7 +5,7 @@
              [:get "/500" :error.server-error/view :500]
              [:post "/" :home.index/action :home/action]
              [:get "/site/:id/assets" :asset.list/view :asset/list]
-             [:get "/sign-up" :auth.signup/view :auth]
-             [:post "/sign-up" :auth.signup/action :auth/action]
-             [:get "/sign-in" :auth.signin/view :auth]
-             [:post "/sign-in" :auth.signin/action]])
+             [:get "/sign-up" :auth.signup/view]
+             [:post "/sign-up" :auth.signup/action]
+             [:get "/sign-in" :auth.login/view]
+             [:post "/sign-in" :auth.login/action]])
