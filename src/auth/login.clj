52639e63b5ce83774207b20b305a67d8f54b58e2
Sean Walker <swlkr@fastmail.com>
2018-10-03 14:39:56 -0700

    Delete commas

--- src/auth/login.clj
+++ src/auth/login.clj
@@ -4,7 +4,7 @@
 
 (defn view [request]
   [:main
-   [:img {:class "auth__logo mw-100", :src "/img/logo-auth.png"}]
+   [:img {:class "auth__logo mw-100" :src "/img/logo-auth.png"}]
    [:div {:class "auth-content"}
     (form (merge (action-for ::action)
                  {:class "auth__form"})
@@ -12,24 +12,24 @@
       [:p "Login"]]
 
      [:div {:class "form__item form__item--full form__item--email"}
-      [:label {:class "form__label", :for "email"}
+      [:label {:class "form__label" :for "email"}
        "Email Address"]
-      [:input {:class "form__input", :type "email", :name "email", :id "email", :required "required"}]
+      [:input {:class "form__input" :type "email" :name "email" :id "email" :required "required"}]
       [:div {:class "form__error"}]]
 
      [:div {:class "form__item form__item--full"}
-      [:label {:class "form__label", :for "password"}
+      [:label {:class "form__label" :for "password"}
        "Password"]
-      [:input {:class "form__input", :type "password", :name "password", :id "password", :required "required"}]
+      [:input {:class "form__input" :type "password" :name "password" :id "password" :required "required"}]
       [:div {:class "form__error"}]]
 
      [:div {:class "form__item form__item--full form__item--actions"}
-      [:input {:class "form__button", :type "submit", :name "Login", :value "Login"}]
-      [:a {:href "login.html", :class "form__forgot-link"}
+      [:input {:class "form__button" :type "submit" :name "Login" :value "Login"}]
+      [:a {:href "login.html" :class "form__forgot-link"}
        "Forgot your password?"]])]
 
 
    [:div {:class "dashed__container"}
     [:p "Don't have an account yet? "
-     [:a {:href (url-for :auth.signup/view), :class "form-link"}
+     [:a {:href (url-for :auth.signup/view) :class "form-link"}
       "Sign Up"]]]])
--- src/auth/signup.clj
+++ src/auth/signup.clj
@@ -3,7 +3,7 @@
 
 (defn view [request]
   [:main
-   [:img {:class "auth__logo mw-100", :src "/img/logo-auth.png"}]
+   [:img {:class "auth__logo mw-100" :src "/img/logo-auth.png"}]
    [:div {:class "auth-content"}
     (form (merge (action-for ::action)
                  {:class "auth__form"})
@@ -11,28 +11,28 @@
       [:p "Create an account"]]
 
      [:div {:class "form__item form__item--full form__item--email"}
-      [:label {:class "form__label", :for "email"}
+      [:label {:class "form__label" :for "email"}
        "Email Address"]
-      [:input {:class "form__input", :type "email", :name "email", :id "email", :required "required"}]
+      [:input {:class "form__input" :type "email" :name "email" :id "email" :required "required"}]
       [:div {:class "form__error"}]]
 
      [:div {:class "form__item"}
-      [:label {:class "form__label", :for "password"}
+      [:label {:class "form__label" :for "password"}
        "Password"]
-      [:input {:class "form__input", :type "password", :name "password", :id "password", :required "required"}]
+      [:input {:class "form__input" :type "password" :name "password" :id "password" :required "required"}]
       [:div {:class "form__error"}]]
 
      [:div {:class "form__item"}
-      [:label {:class "form__label", :for "password"}
+      [:label {:class "form__label" :for "password"}
        "Confirm Password"]
-      [:input {:class "form__input", :type "password", :name "confirm-password", :id "confirm-password", :required "required"}]
+      [:input {:class "form__input" :type "password" :name "confirm-password" :id "confirm-password" :required "required"}]
       [:div {:class "form__error"}]]
 
      [:div {:class "form__item form__item--full form__item--actions"}
-      [:input {:class "form__button", :type "submit", :name "register", :value "Register"}]])]
+      [:input {:class "form__button" :type "submit" :name "register" :value "Register"}]])]
 
 
    [:p {:class "form-link-text"}
     "Already have an account? "
-    [:a {:href (url-for :auth.login/view), :class "form-link"}
+    [:a {:href (url-for :auth.login/view) :class "form-link"}
      "Login"]]])
