b97dab4a8ca80fb2bf73f45f59d5f50be33a9dd5
Sean Walker <swlkr@fastmail.com>
2018-10-04 21:41:32 -0700

    Throw up a prayer to get chromedriver working in prod

--- src/scraper.clj
+++ src/scraper.clj
@@ -41,7 +41,7 @@
                :content (str "Error retrieving content: (" status ") " body)))))
 
 (defn tags! [url]
-  (let [driver (chrome {:headless true :args ["--disable-dev-shm-usage"]})
+  (let [driver (chrome {:headless true :path-driver "/usr/local/bin/chromedriver" :args ["--disable-dev-shm-usage --disable-gpu"]})
         _ (go driver url)
         results (js-execute driver "var elements = document.getElementsByTagName(\"script\");
         var scripts = []
