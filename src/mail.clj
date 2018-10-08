dfbf9d5c262758c707560a59b6e113953cba8dd2
ferrucc-io <hi@ferrucc.io>
2018-10-08 17:40:03 +0200

    Add mailing

---
+++ src/mail.clj
@@ -0,0 +1,16 @@
+(ns notification-mail
+    (:require [postal.core :refer [send-message]])
+    (:gen-class))
+
+(defn send-file-alert [user_email token]
+      (def message_body (str "Hey there,\nIt's the team from Magehash alterting you, we noticed \"filename\" was recently updated, here is the code that changed.\nCheck \"filename\" for these changes!"))
+      (def mail_info {:host "smtp.mailtrap.io"
+                      :user "12341d55f5c1ed"
+                      :pass "2fd6cbf3ccbf1e"})
+
+      (def no-reply-email "canreply@magehash.com")
+
+      (send-message mail_info {:from no-reply-email
+                               :to user_email
+                               :subject "Magehash Alert!"
+                               :body message_body}))
