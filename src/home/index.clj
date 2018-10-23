df37785bd11370257ad71ac47b2ce3d32805f3dd
Sean Walker <swlkr@fastmail.com>
2018-10-23 15:54:42 -0700

    Redirect / -> /dashboard when logged in

--- src/home/index.clj
+++ src/home/index.clj
@@ -42,91 +42,93 @@
    [:img {:class "mw2 pt6 caret_home" :src "/img/caret.svg"}]])
 
 (defn view [request]
-  [:div
-   (hero request)
-   [:div {:class "bg-white pv4 ph3 pv6_mobile" :id "learn-more"}
-    [:div {:class "mb5"}
-       [:div {:class "tc pb5"}
-        [:h4 {:class "f1 explainer-title"} "Why?"
-         [:h3 {:class "f5 tracked gray"} "Recent Breaches In The News"]]]
-       [:div {:class "mt3 cf"}
-        [:div {:class "fl w-third-ns w-100"}
-         [:div {:class "bg-white mid-gray mh2-ns mb4 mb0-ns shadow-4 br2 cf"}
-          [:img {:class "db w-100 br2 br--top" :src "/img/british-airways-600-300.png"}]
-          [:div {:class "pa2 ph3-ns pb3-ns"}
-           [:div {:class "dt w-100 mt1"}
-            [:div {:class "dtc"}
-             [:h4 {:class "f5 f4-ns mv0"} "British Airways"]]]
-           [:p {:class "f6 lh-copy measure mt2 mid-gray"}
-            "British Airways announced it had suffered a breach resulting in the theft of customer data. In interviews with the BBC, the company noted that around 380,000 customers could have been affected and that the stolen information included personal and payment information"]
-           [:cite {:class "f6 tracked fs-normal fr pb3"}
-            [:a {:class "black" :href "https://www.riskiq.com/blog/labs/magecart-british-airways-breach/"}
-              "-RiskIQ"]]]]]
-        [:div {:class "fl w-third-ns w-100"}
-          [:div {:class "bg-white mid-gray mh2-ns mb4 mb0-ns shadow-4 br2 cf"}
-            [:img {:class "db w-100 br2 br--top" :src "/img/tm-600-300.png"}]
+  (if (some? (-> request :session :member/email))
+    (redirect (url-for :dashboard))
+    [:div
+     (hero request)
+     [:div {:class "bg-white pv4 ph3 pv6_mobile" :id "learn-more"}
+      [:div {:class "mb5"}
+         [:div {:class "tc pb5"}
+          [:h4 {:class "f1 explainer-title"} "Why?"
+           [:h3 {:class "f5 tracked gray"} "Recent Breaches In The News"]]]
+         [:div {:class "mt3 cf"}
+          [:div {:class "fl w-third-ns w-100"}
+           [:div {:class "bg-white mid-gray mh2-ns mb4 mb0-ns shadow-4 br2 cf"}
+            [:img {:class "db w-100 br2 br--top" :src "/img/british-airways-600-300.png"}]
             [:div {:class "pa2 ph3-ns pb3-ns"}
-              [:div {:class "dt w-100 mt1"}
-               [:div {:class "dtc"}
-                [:h4 {:class "f5 f4-ns mv0"} "Ticketmaster"]]]
+             [:div {:class "dt w-100 mt1"}
+              [:div {:class "dtc"}
+               [:h4 {:class "f5 f4-ns mv0"} "British Airways"]]]
              [:p {:class "f6 lh-copy measure mt2 mid-gray"}
-              "A recent breach at Ticketmaster was just \"the tip of the iceberg\" of a wider, massive credit card skimming operation, new research has found.
-    At least 800 e-commerce sites are said to be affected, after they included code developed by third-party companies and later altered by hackers"]
+              "British Airways announced it had suffered a breach resulting in the theft of customer data. In interviews with the BBC, the company noted that around 380,000 customers could have been affected and that the stolen information included personal and payment information"]
              [:cite {:class "f6 tracked fs-normal fr pb3"}
               [:a {:class "black" :href "https://www.riskiq.com/blog/labs/magecart-british-airways-breach/"}
-               "-ZDNet"]]]]]
-        [:div {:class "fl w-third-ns w-100"}
-          [:div {:class "bg-white mid-gray mh2-ns mb4 mb0-ns shadow-4 br2 cf"}
-             [:img {:class "db w-100 br2 br--top" :src "/img/ne-600-300.png"}]
-             [:div {:class "pa2 ph3-ns pb3-ns"}
-               [:div {:class "dt w-100 mt1"}
-                [:div {:class "dtc"}
-                  [:h4 {:class "f5 f4-ns mv0"} "Newegg"]]]
-              [:p {:class "f6 lh-copy measure mt2 mid-gray"}
-                  "The breach of Newegg shows the true extent of Magecart operators’ reach. These attacks are not confined to certain geolocations or specific industries—any organization that processes payments online is a target ... they integrated with the victim’s payment system"]
-              [:cite {:class "f6 tracked fs-normal fr pb3"}
-                     [:a {:class "black" :href "https://www.riskiq.com/blog/labs/magecart-newegg/"}
-                         "-RiskIQ"]]]]]]]]
-   [:div {:class "pv6 ph3 bg-lightest-gray pv6_mobile"}
-    [:div {:class "cf mw8 center"}
-       [:div {:class "fl w-50-ns w-100 h5 flex flex-column justify-center"}
-        [:div {:class "ph2"}
-         [:h1 {:class "f3 fw4 lh-title"}
-          "Keep your sites' assets secure"]
-         [:p {:class "f5 lh-copy mid-gray"}
-          "Have confidence that your js assets are exactly as you left them."
-          " No suprises if someone manages to commandeer your CMS or a CDN"]]]
+                "-RiskIQ"]]]]]
+          [:div {:class "fl w-third-ns w-100"}
+            [:div {:class "bg-white mid-gray mh2-ns mb4 mb0-ns shadow-4 br2 cf"}
+              [:img {:class "db w-100 br2 br--top" :src "/img/tm-600-300.png"}]
+              [:div {:class "pa2 ph3-ns pb3-ns"}
+                [:div {:class "dt w-100 mt1"}
+                 [:div {:class "dtc"}
+                  [:h4 {:class "f5 f4-ns mv0"} "Ticketmaster"]]]
+               [:p {:class "f6 lh-copy measure mt2 mid-gray"}
+                "A recent breach at Ticketmaster was just \"the tip of the iceberg\" of a wider, massive credit card skimming operation, new research has found.
+    At least 800 e-commerce sites are said to be affected, after they included code developed by third-party companies and later altered by hackers"]
+               [:cite {:class "f6 tracked fs-normal fr pb3"}
+                [:a {:class "black" :href "https://www.riskiq.com/blog/labs/magecart-british-airways-breach/"}
+                 "-ZDNet"]]]]]
+          [:div {:class "fl w-third-ns w-100"}
+            [:div {:class "bg-white mid-gray mh2-ns mb4 mb0-ns shadow-4 br2 cf"}
+               [:img {:class "db w-100 br2 br--top" :src "/img/ne-600-300.png"}]
+               [:div {:class "pa2 ph3-ns pb3-ns"}
+                 [:div {:class "dt w-100 mt1"}
+                  [:div {:class "dtc"}
+                    [:h4 {:class "f5 f4-ns mv0"} "Newegg"]]]
+                [:p {:class "f6 lh-copy measure mt2 mid-gray"}
+                    "The breach of Newegg shows the true extent of Magecart operators’ reach. These attacks are not confined to certain geolocations or specific industries—any organization that processes payments online is a target ... they integrated with the victim’s payment system"]
+                [:cite {:class "f6 tracked fs-normal fr pb3"}
+                       [:a {:class "black" :href "https://www.riskiq.com/blog/labs/magecart-newegg/"}
+                           "-RiskIQ"]]]]]]]]
+     [:div {:class "pv6 ph3 bg-lightest-gray pv6_mobile"}
+      [:div {:class "cf mw8 center"}
+         [:div {:class "fl w-50-ns w-100 h5 flex flex-column justify-center"}
+          [:div {:class "ph2"}
+           [:h1 {:class "f3 fw4 lh-title"}
+            "Keep your sites' assets secure"]
+           [:p {:class "f5 lh-copy mid-gray"}
+            "Have confidence that your js assets are exactly as you left them."
+            " No suprises if someone manages to commandeer your CMS or a CDN"]]]
+
+         [:div {:class "fl w-50-ns w-100 h5 flex flex-column justify-center"}
+          [:div {:class "ph4-ns"}
+           [:img {:src "/img/safe-lander.svg"}]]]]]
+     [:div {:class "pv6 ph3 pv6_mobile"}
+      [:div {:class "cf mw8 center"}
 
        [:div {:class "fl w-50-ns w-100 h5 flex flex-column justify-center"}
         [:div {:class "ph4-ns"}
-         [:img {:src "/img/safe-lander.svg"}]]]]]
-   [:div {:class "pv6 ph3 pv6_mobile"}
-    [:div {:class "cf mw8 center"}
-
-     [:div {:class "fl w-50-ns w-100 h5 flex flex-column justify-center"}
-      [:div {:class "ph4-ns"}
-       [:img {:src "/img/windows-lander.svg"}]]]
+         [:img {:src "/img/windows-lander.svg"}]]]
 
 
-     [:div {:class "fl w-50-ns w-100 h5 flex flex-column justify-center"}
-      [:div {:class "ph2"}
-       [:h1 {:class "f3 fw4 lh-title"}
-        "Navigate deeply into your site"]
-       [:p {:class "f5 lh-copy mid-gray"}
-        "Let Magehash do the tedious work for you."
-        " Enter your sites in the dashboard and magehash will navigate with sessions to any page on your site, including payment forms, and shopping carts"]]]]]
-   [:div {:class "bg-lightest-gray pv6 ph3 pv6_mobile"}
-    [:div {:class "cf mw8 center"}
-     [:div {:class "fl w-50-ns w-100 h5 flex flex-column justify-center"}
-      [:div {:class "ph2"}
-       [:h1 {:class "f3 fw4 lh-title"}
-        "Get notified of changes by email"]
-       [:p {:class "f5 lh-copy mid-gray"}
-        "Notifications happen when any js on your site changes. If you change your js as part of a deploy, a new hash will be generated automatically and you can safely ignore any warnings"]]]
+       [:div {:class "fl w-50-ns w-100 h5 flex flex-column justify-center"}
+        [:div {:class "ph2"}
+         [:h1 {:class "f3 fw4 lh-title"}
+          "Navigate deeply into your site"]
+         [:p {:class "f5 lh-copy mid-gray"}
+          "Let Magehash do the tedious work for you."
+          " Enter your sites in the dashboard and magehash will navigate with sessions to any page on your site, including payment forms, and shopping carts"]]]]]
+     [:div {:class "bg-lightest-gray pv6 ph3 pv6_mobile"}
+      [:div {:class "cf mw8 center"}
+       [:div {:class "fl w-50-ns w-100 h5 flex flex-column justify-center"}
+        [:div {:class "ph2"}
+         [:h1 {:class "f3 fw4 lh-title"}
+          "Get notified of changes by email"]
+         [:p {:class "f5 lh-copy mid-gray"}
+          "Notifications happen when any js on your site changes. If you change your js as part of a deploy, a new hash will be generated automatically and you can safely ignore any warnings"]]]
 
-     [:div {:class "fl w-50-ns w-100 h5 flex flex-column justify-center"}
-      [:div {:class "ph4-ns"}
-       [:img {:src "/img/sync-lander.svg"}]]]]]])
+       [:div {:class "fl w-50-ns w-100 h5 flex flex-column justify-center"}
+        [:div {:class "ph4-ns"}
+         [:img {:src "/img/sync-lander.svg"}]]]]]]))
 
 (defn action [request]
   (let [[site errors] (-> (:params request)
